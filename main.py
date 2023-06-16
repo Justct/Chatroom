cf9965c450f29131d6b8dea0ba541e65dcfee6ba
Cactochan <85914469+merwin-asm@users.noreply.github.com>
2023-06-16 21:04:15 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+import subprocess
+import os
+import platform
+
+
+def start_server(ip, port):
+    if platform.system().lower() == "windows":
+        os.system(f"python3 api.py {ip} {port}")
+
+def start_ngrok(port, use_https):
+    print(f"Starting Ngrok on port {port}")
+    if use_https:
+        subprocess.run(["ngrok", "http", port])
+    else:
+        subprocess.run(["ngrok", "tcp", port])
+
+def main():
+    ip = input("Enter the IP address to run the API (default '0.0.0.0'): ")
+    ip = ip.replace(" ", "")
+    if ip  == "":
+        ip = "0.0.0.0"
+
+    try:
+        port =int( input("Enter the port number (default 5010): "))
+        port = port.replace(" ", "")
+        if port == "":
+            port = "5010"
+    except:
+        port = "5010"
+
+    use_ngrok = input("Do you want to use Ngrok for port forwarding? (y/N): ").lower() == "y"
+    if use_ngrok:
+        use_https = input("Do you want to use HTTPS or TCP? [using HTTPS provided by ngrok will expose your IP] (y/N): ").lower() == "y"
+
+    if use_ngrok:
+        start_ngrok(port, use_https)
+
+    start_server(ip, port)
+
+if __name__ == "__main__":
+    main()
