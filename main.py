72f4e04843b4183a44a8c14f8d8317a194a9bb8f
Cactochan <85914469+merwin-asm@users.noreply.github.com>
2023-06-17 20:14:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,69 +1,41 @@
-import subprocess
 import os
-import platform
 import requests
-import time
 import json
-
+import platform
 
 room_name = input("Room Name  : ")
-room_url = input("Room URL : ")
+room_url = input("Room Domain/Public IP (if ip do ip:8069) : ")
 
 f = open("info.json","w")
 f.write(json.dumps({"name":room_name, "url" : room_url}))
 f.close()
 
-
 def P():
     response = requests.get(f"https://backendjustchat.darkmash.repl.co/find/{room_name}")
     return response.text == room_url
 
+if P():
+    if platform.system().lower() == "windows":
+        os.system(f"python app.py 0.0.0.0 8069")
+    else:
+        os.system(f"python3 api.py 0.0.0.0 8069")
+
 
 def start_server(ip, port):
     if not P():
-        if platform.system().lower() == "linux":
-            os.system(f"python3 verify.py {ip} {port}")
+        if platform.system().lower() == "windows":
+            os.system(f"python verify.py {ip} {port}")
         else:
-            os.system(f"python verify.py {ip} {port}")      
-    print("WAIT 10 SECS")
-    time.sleep(10)
-    if not P():
-        print("The room url and the url provided didnt match or someother error!! Quiting")
-        quit()
+            os.system(f"python3 verify.py {ip} {port}")      
 
-    if platform.system().lower() == "linux":
-        os.system(f"python3 api.py {ip} {port}")
+    if platform.system().lower() == "windows":
+        os.system(f"python app.py 0.0.0.0 8069")
     else:
-        os.system(f"python Chatroom/api.py {ip} {port}")
-def start_ngrok(port, use_https):
-    print(f"Starting Ngrok on port {port}")
-    if use_https:
-        subprocess.run(["ngrok", "http", port])
-    else:
-        subprocess.run(["ngrok", "tcp", port])
+        os.system(f"python3 api.py 0.0.0.0 8069")
+
 
 def main():
-    ip = input("Enter the IP address to run the API (default '0.0.0.0'): ")
-    ip = ip.replace(" ", "")
-    if ip  == "":
-        ip = "0.0.0.0"
-
-    try:
-        port =int( input("Enter the port number (default 5010): "))
-        port = port.replace(" ", "")
-        if port == "":
-            port = "5010"
-    except:
-        port = "5010"
-
-    use_ngrok = input("Do you want to use Ngrok for port forwarding? (y/N): ").lower() == "y"
-    if use_ngrok:
-        use_https = input("Do you want to use HTTPS or TCP? [using HTTPS provided by ngrok will expose your IP] (y/N): ").lower() == "y"
-
-    if use_ngrok:
-        start_ngrok(port, use_https)
-
-    start_server(ip, port)
-
+    start_server("0.0.0.0", 8069)
+   
 if __name__ == "__main__":
     main()
