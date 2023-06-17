07d0b1f8ecea75199e61a27d21ea2d0706387bf4
Cactochan <85914469+merwin-asm@users.noreply.github.com>
2023-06-17 11:41:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import subprocess
 import os
 import platform
+import requests
+import time
+
+
+room_name = input("Room Name  : ")
+room_url = input("Room URL : ")
+
+def P():
+    response = requests.get(f"https://backendjustchat.darkmash.repl.co/find/{room_name}")
+    return response.text == room_url
 
 
 def start_server(ip, port):
+    if not P():
+        if platform.system().lower() == "linux":
+            os.system(f"python3 verify.py {ip} {port}")
+        else:
+            os.system(f"python verify.py {ip} {port}")      
+    time.sleep(10)
+    if not P():
+        print("The room url and the url provided didnt match or someother error!! Quiting")
+        quit()
+
     if platform.system().lower() == "linux":
-        os.system(f"python3 Chatroom/api.py {ip} {port}")
+        os.system(f"python3 api.py {ip} {port}")
     else:
         os.system(f"python Chatroom/api.py {ip} {port}")
 def start_ngrok(port, use_https):
